f875fbb4f5e6eec8e92b2f8ed3abe63899fc7cd2
Matt-ags <mateusabnelocca@gmail.com>
2025-09-01 15:16:05 -0300

    base para integração entre apps feed e autenticação feita / acesso aos dados cadastrados ok

--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User # na docs, mostra que já temos "criado", usamos essa model como base :D
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
+from usuarios.models import *
 # Create your views here.
+@login_required(login_url='/auth/login/')
+def plataforma(request):
+
+    # BUSCANDO DADOS DO USUARIO: 
+
+    user_username = request.user.username # NOME
+    user_user_id = request.user.id # ID
+    dados_user = Perfil.objects.filter(user_id=user_user_id) # SELECT COM OUTRAS INFORMAÇÕES
+    # print(user_username)
+    # print(user_user_id)
+    # print(dados_user)
+    # for dado in dados_user:
+    #     print(dado.poderes)
+    #     print(dado.descricao)
+
+    if dados_user: # SE CONSEGUIU ACHAR
+        user_infs = dados_user[0] # PEGA O QUE ACHOU (é confuso, mas só estamos acessando o dado, que no caso, o primeiro é o que importa)
+
+        poderes = user_infs.poderes # poderes
+        descricao = user_infs.descricao # descrição
+    
+    usuario_cadastrado = { # objeto com as informações bases
+        'nome_usuario': user_username,
+        'id_usuario': user_user_id,
+        'dadosbrutos_usuario': dados_user,
+        'poderes_usuario': poderes,
+        'descricao_usuario': descricao
+    }
+    
+    return HttpResponse("ok plataforma (a partir do app feed)")
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from feed import views as feed_views
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login, name='login'),
-    path('plataforma/', views.plataforma, name='plataforma'),
+    path('plataforma/', feed_views.plataforma, name='plataforma'), # vou realizar desta forma, a partir desse app, eu importo o app feed.
 ]
