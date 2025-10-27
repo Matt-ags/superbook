80f6204bbd958bf995b81467c2a84a1fb3ab115d
Matt-ags <mateusabnelocca@gmail.com>
2025-10-27 14:45:58 -0300

    test url 2

--- superbook/urls.py
+++ superbook/urls.py
@@ -1,19 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
-from usuarios import views
+# from usuarios import views  <-- NÃO PRECISAMOS MAIS DESTA LINHA
 from django.conf import settings
 from django.conf.urls.static import static
 
 from django.views.static import serve
 from django.urls import re_path
 
+from django.views.generic import RedirectView
+
 urlpatterns = [
-    path('', include('usuarios.urls')),
+    path('', RedirectView.as_view(url='/auth/login/', permanent=True), name='home'),
+    
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), # <-- Adicione esta linha
 
     # apps
-    # path('auth/', include('usuarios.urls')),
+    path('auth/', include('usuarios.urls')),
     path('plataforma/', include('feed.urls')),
     path('plataforma/perfil/', include('perfil.urls')),
     path('plataforma/posts/', include('comentarios.urls')),
@@ -22,12 +25,11 @@ urlpatterns = [
     path('viloes/', include('viloes.urls'))
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
-    # --- HACK PARA SERVIR MÍDIA TEMPORÁRIA NO RENDER ---
-    # Isso é inseguro e ineficiente para produção real,
-    # mas funciona para "ver a lógica".
+
     urlpatterns += [
         re_path(r'^media/(?P<path>.*)$', serve, {
             'document_root': settings.MEDIA_ROOT,
